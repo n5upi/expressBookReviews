1459a94b2b6396b89bf11677ee1dcecf162b1479
n5upi <dallasats@hotmail.com>
2024-03-19 03:48:15 -0400

    Adding temporary changes to Github

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,7 +7,19 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!doesExist(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});    
+    }
+  } 
+  return res.status(404).json({message: "Unable to register user."});
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
@@ -16,27 +28,90 @@ public_users.get('/',function (req, res) {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get book details based on ISBN
+// Get book details based on ISBN (TASK 11)
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const get_books_isbn = new Promise((resolve, reject) => {
+    const isbn = req.params.isbn;
+    // console.log(isbn);
+        if (req.params.isbn <= 10) {
+        resolve(res.send(books[isbn]));
+    }
+        else {
+            reject(res.send('ISBN not found'));
+        }
+    });
+    get_books_isbn.
+        then(function(){
+            console.log("Promise for Task 11 is resolved");
+   }).
+        catch(function () { 
+                console.log('ISBN not found');
+  });
+  //return res.status(300).json({message: "Yet to be implemented"});
  });
   
-// Get book details based on author
+// Get book details based on author (TASK 12)
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const get_books_author = new Promise((resolve, reject) => {
+
+    let booksbyauthor = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["author"] === req.params.author) {
+        booksbyauthor.push({"isbn":isbn,
+                            "title":books[isbn]["title"],
+                            "reviews":books[isbn]["reviews"]});
+      resolve(res.send(JSON.stringify({booksbyauthor}, null, 4)));
+      }
+
+
+    });
+    reject(res.send("The mentioned author does not exist "))
+
+    });
+
+    get_books_author.then(function(){
+            console.log("Promise is resolved");
+   }).catch(function () { 
+                console.log('The mentioned author does not exist');
+  });
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get all books based on title
+// Get all books based on title (TASK 13)
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const get_books_title = new Promise((resolve, reject) => {
+
+    let booksbytitle = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["title"] === req.params.title) {
+        booksbytitle.push({"isbn":isbn,
+                            "author":books[isbn]["author"],
+                            "reviews":books[isbn]["reviews"]});
+    resolve(res.send(JSON.stringify({booksbytitle}, null, 4)));
+      }
+    });
+
+    reject(res.send("The mentioned title does not exist "))
+
+       });
+
+    get_books_title.then(function(){
+            console.log("Promise is resolved");
+   }).catch(function () { 
+                console.log('The mentioned book title doesnt exist');
+  });
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
+
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
